5a042732387db72374d1e1e884a6409efca43e86
Alex Alvarez Perez <aalvarezperez@ebay.com>
2021-06-10 10:45:00 +0200

    finished notes and example

--- estimate_cate_function.R
+++ estimate_cate_function.R
@@ -1,7 +1,5 @@
 
-fit_learner. <- function(y, w, X, learner){
-  n_folds <- 5
-  num_search_rounds <- 10
+fit_learner. <- function(y, w, X, learner, num_search_rounds, n_folds){
   switch(
     learner,
     S = sboost(
@@ -31,11 +29,11 @@ fit_learner. <- function(y, w, X, learner){
       num_search_rounds = num_search_rounds,
       verbose = FALSE
     ),
-    R.cf = grf::causal_forest(
+    causal_forest = grf::causal_forest(
       Y = y,
       X = X,
       W = w, 
-      tune.parameters = "all", 
+      tune.parameters = "all",
       )
   )
 }
@@ -88,7 +86,14 @@ sanitise_data. <- function(data, args){
 
 
 
-estimate_heterogenous_effects <- function(data, y, w, X, test_group, sample_prop = NULL, learner = c("S", "T", "X", "R", "R.cf")){
+estimate_heterogenous_effects <- function(data,
+                                          y, w, X,
+                                          test_group, 
+                                          sample_prop = NULL,
+                                          num_search_rounds = 10,
+                                          n_folds = 3,
+                                          learner = c("S", "T", "X", "R", "causal_forest")
+                                          ){
   
   require(rlearner)
   require(dplyr)
@@ -109,103 +114,3 @@ estimate_heterogenous_effects <- function(data, y, w, X, test_group, sample_prop
 
   fit_learner.(y, w, X, learner)
 }
-
-covs <- c("fixed_acidity", "volatile_acidity", "citric_acid", 
-  "residual_sugar", "chlorides", "free_sulfur_dioxide", "total_sulfur_dioxide", 
-  "density", "pH", "sulphates", "alcohol")
-
-x_test <- estimate_heterogenous_effects(
-  data = dat_clean,
-  y = "quality",
-  w = "color",
-  test_group = "red",
-  X = covs,
-  learner = "X"
-  # sample_prop = .1
-  )
-
-r_test <- estimate_heterogenous_effects(
-  data = dat_clean,
-  y = "quality",
-  w = "color",
-  test_group = "red",
-  X = covs,
-  learner = "R.cf"
-)
-
-r_test2 <- estimate_heterogenous_effects(
-  data = dat_clean,
-  y = "quality",
-  w = "color",
-  test_group = "red",
-  X = covs,
-  learner = "R"
-)
-
-
-
-
-df <- MatchIt::lalonde
-df <- fastDummies::dummy_cols(df) %>%
-  select(-race)
-names(df)
-
-covs2 <- c("age", "educ", "married", "nodegree", "re74", "re75", "race_black", "race_hispan", "race_white")
-tau_lalonde <- estimate_heterogenous_effects(
-  data = df,
-  y = "re78",
-  w = "treat",
-  test_group = 1,
-  X = covs2,
-  learner = "R.cf"
-)
-
-
-tau_lalonde$predictions %>% 
-  hist(breaks = 50)
-
-mean(predict(tau_lalonde, as.matrix(df[covs2]))$predictions)
-
-
-
-# Train a standard survival forest.
-n <- 2000
-p <- 5
-X <- matrix(rnorm(n * p), n, p)
-failure.time <- exp(0.5 * X[, 1]) * rexp(n)
-censor.time <- 2 * rexp(n)
-Y <- pmin(failure.time, censor.time)
-D <- as.integer(failure.time <= censor.time)
-s.forest <- grf::survival_forest(X, Y, D)
-
-# Predict using the forest.
-X.test <- matrix(0, 3, p)
-X.test[, 1] <- seq(-2, 2, length.out = 3)
-s.pred <- predict(s.forest, X.test)
-
-# Plot the survival curve.
-plot(NA, NA, xlab = "failure time", ylab = "survival function",
-     xlim = range(s.pred$failure.times),
-     ylim = c(0, 1))
-for(i in 1:3) {
-  lines(s.pred$failure.times, s.pred$predictions[i,], col = i)
-  s.true = exp(-s.pred$failure.times / exp(0.5 * X.test[i, 1]))
-  lines(s.pred$failure.times, s.true, col = i, lty = 2)
-}
-
-# Predict on out-of-bag training samples.
-s.pred <- predict(s.forest)
-
-# Plot the survival curve for the first five individuals.
-matplot(s.pred$failure.times, t(s.pred$predictions[1:5, ]),
-        xlab = "failure time", ylab = "survival function (OOB)",
-        type = "l", lty = 1)
-
-# Train the forest on a less granular grid.
-failure.summary <- summary(Y[D == 1])
-events <- seq(failure.summary["Min."], failure.summary["Max."], by = 0.1)
-s.forest.grid <- grf::survival_forest(X, Y, D, failure.times = events)
-s.pred.grid <- predict(s.forest.grid)
-matpoints(s.pred.grid$failure.times, t(s.pred.grid$predictions[1:5, ]),
-          type = "l", lty = 2)
-
--- scripts/example.R
+++ scripts/example.R
@@ -1,28 +1,96 @@
 require(pacman)
-p_load(dplyr)
 source("estimate_cate_function.R")
+p_load(
+  dplyr,
+  DALEX,
+  grf
+  )
 
+################
+#### Data ######
+################
+
+# we work with lalonde today
 lalonde <- MatchIt::lalonde
+
+# create dummies for 'race'. 
 lalonde <- fastDummies::dummy_cols(lalonde) %>%
   select(-race)
 
+# check that
 names(lalonde)
 
 covs <- c("age", "educ", "married", "nodegree", "re74", "re75", "race_black", "race_hispan", "race_white")
 
+
+
+################# 
+##### Model #####
+#################
+
+# this wraps around several methods to estimate CATE. We use causal_forest today.
 tau_lalonde <- estimate_heterogenous_effects(
   data = lalonde,
   y = "re78",
   w = "treat",
   test_group = 1,
   X = covs,
-  learner = "R.cf"
+  learner = "causal_forest"
 )
 
+# here are our estimated effects for each subject
+predict(tau_lalonde)
 preds <- tau_lalonde$predictions 
+lalonde$effect <- preds
 preds %>% hist(breaks = 50)
-mean(preds)
-mean(preds > 0)
 
-grf::best_linear_projection(forest = tau_lalonde, A = lalonde[covs])
 
+
+##################################
+#### Inference and explanation ###
+##################################
+
+# we can answer right away ...
+grf::average_treatment_effect(tau_lalonde, target.sample = "all") # ... what is the ATE, as the average CATE 
+mean(preds > 0) #... what share of the subjects are estimated to have a 'positive' impact of the treatment (100% here)
+
+# or we can summarise the CATE functions as tau(x) = E[Y(1) - Y(0) | X] with a linear model:
+# tau(X) ~ beta0 + A * beta. This tells us how CATE relates to some variables.
+best_linear_projection(tau_lalonde, lalonde[covs]) # here we use the same ones we used for the causal forest.
+
+
+# we can explain CATE in other ways too: https://dalex.drwhy.ai/ ...
+
+# create an explainer for the function of CATE
+explainer_cate <- explain(
+  model = tau_lalonde, # model that learned the function of CATE
+  data = lalonde, # ideally this is unseen data
+  y = lalonde$effect, # the estimated effect. note: this can never be the ground truth. that does not exist. We can only estimate it.
+  predict_function = function(model, newdata) predict(model, newdata[covs])$predictions, 
+  label = "causal forest"
+    )
+
+
+# explain on dataset level:
+plot(predict_profile(explainer_cate, new_observation = lalonde[2,covs], variables = covs, type = "ceteris_paribus")) # age seems to be important
+plot(variable_profile(explainer_cate, variables = "age")) # subjects above 20 years see a positive impact on their susceptibility to treatment
+
+# explain on subject-level
+harry <- harry_made_younger <- lalonde[3,covs] # the third obersation in the dataset
+harry_made_younger$age <- 20 # surgical intervention on age
+
+# harry prediction
+plot(predict_parts(explainer_cate, new_observation = harry, type = "break_down_interactions")) 
+
+# harry's prediction, had he been 20 years, instead of 30
+plot(predict_parts(explainer_cate, new_observation = harry_made_younger, type = "break_down_interactions"))
+
+
+# These methods offer mainly 2 advantages for AB testing in eCG:
+# - insight in impact heterogeneity of product changes
+#   - statements like '40% of the users saw a negative impact' can be made, or;
+#   - '40% of the users saw a negative impact, even if on average we saw a positive effect'
+#   - '80% of the estimated gain in metric x due to treatment T, sits in 20% of the users'
+# - explain heterogeneity by user characterisitcs
+#   - statements like 'we see that users who had less than 10 VIPs in interval p before treatment, show lower benefit from the treatment than their complement'
+#   - opens the door for personalised recommendations of treatment: rolling out different versions of a product for different users
